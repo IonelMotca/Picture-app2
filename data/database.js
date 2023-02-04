f34747bedf45c8237f6d33a3e8bbde10310cb172
Motca Ionel <motca.ionel@ibm.com>
2023-02-04 12:47:56 +0200

    first commit

--- data/database.js
+++ data/database.js
@@ -2,7 +2,8 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
-let mongodbUrl = 'mongodb://127.0.0.1:27017';
+// let mongodbUrl = 'mongodb://127.0.0.1:27017';
+ let mongodbUrl = 'mongodb://mongo:pyIl6GGXCsWmpOHYP7FY@containers-us-west-151.railway.app:7206';
 
 if(process.env.MONGODB_URL){
   mongodbUrl = process.env.MONGODB_URL;
